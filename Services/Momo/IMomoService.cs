0d3698f9850c45465f79110eb47cee36cc9c73ee
nghiadarealest <145700377+nghiadarealest@users.noreply.github.com>
2025-03-08 12:10:05 +0700

    Create IMomoService.cs

---
+++ Services/Momo/IMomoService.cs
@@ -0,0 +1,10 @@
+using _123.Models.OrderMomo;
+using _123.Models.Momo;
+namespace _123.Services.Momo
+{
+    public interface IMomoService
+    {
+        Task<MomoCreatePaymentResponseModel> CreatePaymentAsync(OrderInfoModel model);
+        MomoExecuteResponseModel PaymentExecuteAsync(IQueryCollection collection);
+    }
+}
