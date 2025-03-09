4fa266035b6dfa282ca5d2f07c0de518e419b9e9
Kok9113 <163729287+Kok9113@users.noreply.github.com>
2025-03-09 23:02:22 +0700

    Delete Services/ViewModel directory

--- Services/ViewModel/MaterialViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _123 {
-  public class MaterialViewModel
-  {
-    public List<Material> Materials {get; set;}
-
-    public Material Material {get; set;}
-
-  }
-}
--- Services/ViewModel/VNpay/IVnPayService.cs
+++
@@ -1,8 +0,0 @@
-using _123.Models.VNPay;
-
-namespace _123.Services.VNPay;
-public interface IVnPayService
-{
-    string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
-    PaymentResponseModel PaymentExecute(IQueryCollection collections);
-}
